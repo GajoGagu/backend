588108b51fd721aad15ec2a3e7e99edd2fb44e80
g-yunjh <yunjh1126@naver.com>
2025-10-10 21:05:30 +0900

    chore: remove unused db

--- services/crud-api/database/__init__.py
+++ services/crud-api/database/__init__.py
@@ -1,11 +1,11 @@
 from .storage import (
-    users_db, products_db, categories_db,
-    wishlist_db, cart_db, orders_db, notifications_db,
+    users_db, products_db,
+    wishlist_db, orders_db, notifications_db,
     active_tokens, init_sample_data
 )
 
 __all__ = [
-    "users_db", "products_db", "categories_db",
-    "wishlist_db", "cart_db", "orders_db", "notifications_db",
+    "users_db", "products_db",
+    "wishlist_db", "orders_db", "notifications_db",
     "active_tokens", "init_sample_data"
 ]
